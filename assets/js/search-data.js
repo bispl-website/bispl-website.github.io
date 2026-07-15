fdb4d2b9db8683855a74f6293a502146c35e0a06
bispl-website <bisplhome@gmail.com>
2026-07-15 09:33:58 +0000

    Deploying to gh-pages from @ bispl-website/bispl-website.github.io@9e251f47dcb6205a49c29a7aeda4f441a5c8b775 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "People",
-          description: "Current members of BISPL @ KAIST AI.",
+          description: "Current members of BISPL @ KAIST.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
